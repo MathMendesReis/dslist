f4b06a055d36a6b31153570e86d657c78fd25108
Matheus Mendes <matheusmendesreis@outlook.com>
2023-05-10 15:37:09 -0300

    game seed

--- src/main/java/com/example/dslist/entidades/Game.java
+++ src/main/java/com/example/dslist/entidades/Game.java
@@ -1,102 +1,131 @@
 package com.example.dslist.entidades;
 
+
 import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.GenerationType;
-
-
-
 
 @Entity
-// A ANOTATION ENTITY TORNA A CLASSE EQUIVALENTE A AUMA TABELA SQL
 @Table(name = "tb_game")
 public class Game {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
 
-    private long id;
-    private String title;
-    @Column(name="game_year")
-    private String year;
-    private String genre;
-    private String platform;
-    private String imgUrl;
-    private String shortDescription;
-    private String longDescription;
-    public long getId() {
-        return id;
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
+	
+    public Game() {
     }
-    public void setId(long id) {
-        this.id = id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getYear() {
-        return year;
-    }
-    public void setYear(String year) {
-        this.year = year;
-    }
-    public String getGenre() {
-        return genre;
-    }
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-    public String getPlatform() {
-        return platform;
-    }
-    public void setPlatform(String platform) {
-        this.platform = platform;
-    }
-    public String getImgUrl() {
-        return imgUrl;
-    }
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
-    public String getShortDescription() {
-        return shortDescription;
-    }
-    public void setShortDescription(String shortDescription) {
-        this.shortDescription = shortDescription;
-    }
-    public String getLongDescription() {
-        return longDescription;
-    }
-    public void setLongDescription(String longDescription) {
-        this.longDescription = longDescription;
-    }
-    public Game(long id, String title, String year, String genre, String platform, String imgUrl,
-            String shortDescription, String longDescription) {
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.genre = genre;
-        this.platform = platform;
-        this.imgUrl = imgUrl;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
-    }
-    public void game(){
+    
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
+		this.id = id;
+		this.title = title;
+		this.year = year;
+		this.genre = genre;
+		this.platforms = platforms;
+		this.score = score;
+		this.imgUrl = imgUrl;
+		this.shortDescription = shortDescription;
+		this.longDescription = longDescription;		
+	}
 
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
+	public Long getId() {
+		return id;
+	}
 
-    @Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getPlatforms() {
+		return platforms;
+	}
+
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getLongDescription() {
+		return longDescription;
+	}
+
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -107,5 +136,4 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-    
 }
